e0d34b9ede38c7235902ada2e92a8b2156ce3f90
boonebytes <boonebytes@users.noreply.github.com>
2021-11-29 20:19:36 -0330

    Added missing files

---
+++ Domain/Entities/Admin/DirectMessage.Aggregate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DiscordBot.Domain.Seedwork;
+
+namespace DiscordBot.Domain.Entities.Admin
+{
+    public partial class DirectMessage : IAggregateRoot
+    {
+        public DirectMessage(
+            ulong fromUser,
+            string message)
+        {
+            ReceivedTimestamp = DateTime.UtcNow;
+            FromUser = fromUser;
+            Message = message;
+        }
+
+        public void AddServer(ulong guildId, string name)
+        {
+            var entry = name + " (" + guildId.ToString() + ")";
+            if (string.IsNullOrEmpty(CommonServers))
+            {
+                CommonServers = entry;
+            }
+            else
+            {
+                CommonServers += ", " + entry;
+            }
+        }
+    }
+}
---
+++ Domain/Entities/Admin/DirectMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DiscordBot.Domain.Seedwork;
+
+namespace DiscordBot.Domain.Entities.Admin
+{
+    public partial class DirectMessage : Entity
+    {
+        public DirectMessage()
+        {
+            ReceivedTimestamp = DateTime.UtcNow;
+        }
+
+        public virtual DateTime ReceivedTimestamp { get; private set; }
+        public virtual ulong FromUser { get; private set; }
+        public virtual string CommonServers { get; private set; }
+        public virtual string Message { get; private set; }
+    }
+}
