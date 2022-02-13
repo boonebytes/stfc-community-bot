51e13343d7502c4b0fcdda86f80e9759b6457c2a
boonebytes <boonebytes@users.noreply.github.com>
2022-02-13 06:09:27 -0330

    Initial draft of audit log functionality

---
+++ Domain/Seedwork/AuditType.cs
@@ -0,0 +1,9 @@
+namespace DiscordBot.Domain.Seedwork;
+
+public enum AuditType
+{
+    None = 0,
+    Create = 1,
+    Update = 2,
+    Delete = 3
+}
---
+++ Infrastructure/EntityConfigurations/AuditEntityTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DiscordBot.Domain.Entities.Alliances;
+using DiscordBot.Domain.Entities.Zones;
+using DiscordBot.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DiscordBot.Infrastructure.EntityConfigurations
+{
+    public class AuditEntityTypeConfiguration : IEntityTypeConfiguration<Audit>
+    {
+        public void Configure(EntityTypeBuilder<Audit> auditConfiguration)
+        {
+            auditConfiguration.ToTable("audit");
+
+            auditConfiguration.HasKey(a => a.Id);
+            auditConfiguration.Property(a => a.Id)
+                .ValueGeneratedOnAdd();
+
+            auditConfiguration.Property(a => a.Type)
+                .HasMaxLength(50);
+            auditConfiguration.Property(a => a.TableName)
+                .HasMaxLength(200);
+            auditConfiguration.Property(a => a.UserId)
+                .HasMaxLength(200);
+        }
+    }
+}
